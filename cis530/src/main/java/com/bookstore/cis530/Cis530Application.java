99770a7feb522f38954bb7f106f91742d3758450
Nolan Eitzen <neitzen@my365.bellevue.edu>
2024-03-16 20:11:49 -0500

    Initial Commit

---
+++ cis530/src/main/java/com/bookstore/cis530/Cis530Application.java
@@ -0,0 +1,13 @@
+package com.bookstore.cis530;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cis530Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cis530Application.class, args);
+	}
+
+}
---
+++ cis530/src/test/java/com/bookstore/cis530/Cis530ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstore.cis530;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cis530ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
